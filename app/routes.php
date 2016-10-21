9bd6ce3826ff0fd7b9838902aa07488e074c0757
yezuozuo <1062276629@qq.com>
2016-10-21 10:25:06 +0800

    table

--- app/routes.php
+++ app/routes.php
@@ -2,30 +2,31 @@
 
 $app->get('/','HomeController:index');
 
-/**
- * lab
- */
-$app->get('/db','HomeController:db');
+/** lab **/
+$app->get('/lab/db','HomeController:db');
 
-$app->get('/contact',function ($request, $response){
+$app->get('/lab/contact',function ($request, $response){
     return $this->view->render($response,'/module/contacts.twig');
 });
 
-$app->get('/echarts',function ($request, $response){
+$app->get('/lab/echarts',function ($request, $response){
     return $this->view->render($response,'/module/charts/echarts.twig');
 });
 
-$app->get('/echarts/bar',function ($request, $response){
+$app->get('/lab/echarts/bar',function ($request, $response){
     return $this->view->render($response,'/module/charts/bar.twig');
 });
 
-$app->get('/echarts/line',function ($request, $response){
+$app->get('/lab/echarts/line',function ($request, $response){
     return $this->view->render($response,'/module/charts/line.twig');
 });
 
-$app->get('/echarts/pie',function ($request, $response){
+$app->get('/lab/echarts/pie',function ($request, $response){
     return $this->view->render($response,'/module/charts/pie.twig');
 });
-/**
- * end lab
- */
+
+$app->get('/lab/table',function ($request, $response){
+    return $this->view->render($response,'/module/table.twig');
+});
+
+/** end lab **/
